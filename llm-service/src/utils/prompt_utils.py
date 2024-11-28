3e0cd393fd37cc510123547bdf9e9904eb603445
JAASPERlegaltech <info@jaasper.com>
2024-11-28 14:13:44 +0100

    upd: RAG prompt

--- llm-service/src/utils/prompt_utils.py
+++ llm-service/src/utils/prompt_utils.py
@@ -6,6 +6,7 @@ Informationen aus dem EU AI Act sind unten angeführt.
 {context_str}
 Basierend auf den Informationen und ohne Vorwissen beantworte die Anfrage. Antworte faktenbasiert und ohne Konklusionen.
 Antworte in der selben Sprache, in der die Anfrage geschrieben ist.
+Falls diese Begriffe in deiner Antwort vorkommen, beachte die Artikel der folgenden Begriffe: der EU AI Act, das LLM, das Large Language Modell, die KI, die DSGVO.
 Anfrage: {query_str}
 Antwort:
 """
