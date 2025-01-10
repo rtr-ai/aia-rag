4479734024aa8cbb64f1efe8752a5bb6c2e52192
Bojan Makivic <59933743+BojanMakivic@users.noreply.github.com>
2025-01-10 08:41:09 +0000

    new

--- auto_test/app.py
+++ auto_test/app.py
@@ -17,30 +17,22 @@ def index():
 
 @app.route("/compare/<question>")
 def compare(question):
-    question_text = question.replace("_", " ")  # Handle spaces in URLs if necessary
+    evaluated = set(evaluated_results.get(question, []))
+    true = set(true_results.get(question, []))
     
-    # Load the evaluated and true JSONs
-    evaluated = set([normalize_string(chunk) for chunk in evaluated_results.get(question_text, [])])
-    true = set([normalize_string(chunk) for chunk in true_results.get(question_text, [])])
-    
-    # Calculate matched, non-matched, and missing chunks
     matched = evaluated & true
     non_matched = evaluated - true
     missing = true - evaluated
     
     return render_template(
         "compare.html",
-        question=question_text,
+        question=question,
         matched=list(matched),
         non_matched=list(non_matched),
         missing=list(missing),
         evaluated=list(evaluated),
         true=list(true),
     )
-
-def normalize_string(s):
-    """Normalize strings by removing special characters, extra spaces, and making them lowercase."""
-    return " ".join(s.lower().split()).replace("„", "").replace("“", "")
 
 if __name__ == "__main__":
     app.run(debug=True)
