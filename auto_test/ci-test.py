ca975bf9114b2e874df63d3b4a8ece6a9f816e2f
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-02-22 09:51:50 +0100

    adjust ci

--- auto_test/ci-test.py
+++ auto_test/ci-test.py
@@ -3,9 +3,11 @@ import os
 from time import sleep
 
 import requests
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 MAX_RETRIES = 10
-TIMEOUT = 200
+TIMEOUT = 10
 
 URL = "https://rag.ki.rtr.at/llm-service/chat"
 HEADERS = {
@@ -25,6 +27,7 @@ for question in ["Ist der AIA ausserhalb der EU anwendbar?"]:
                 "POST", URL, headers=HEADERS, data=payload, stream=True, timeout=TIMEOUT
             )
             if response.status_code != 200:
+                logging.info("return code " + str(response.status_code))
                 raise requests.exceptions.ConnectionError
 
             full_response = ""
@@ -33,13 +36,14 @@ for question in ["Ist der AIA ausserhalb der EU anwendbar?"]:
                     decoded_line = line.decode('utf-8').strip()
                     if decoded_line.startswith("data:"):
                         json_data = json.loads(decoded_line[5:].strip())
-                        print("got " + json_data.get("type"))
+                        logging.info("got " + json_data.get("type"))
                         if json_data.get("type") == "assistant":
                             exit(0)
         except (requests.exceptions.ChunkedEncodingError,
                 requests.exceptions.ConnectionError) as e:
             attempt += 1
-            print("retrying " + str(attempt))
-            if attempt > MAX_RETRIES:
-                exit(1)
+            logging.info("retrying " + str(attempt))
             sleep(10)
+
+logging.warning("maximum retries reached, exiting")
+exit(1)
