3dad14846e85c009341d3ab2f328d1873b37b1e2
Thomas Schreiber <thomas.schreiber@gmx.at>
2025-03-05 11:01:10 +0100

    try restoring ci e2e test functionality

---
+++ .github/ci-test.py
@@ -0,0 +1,49 @@
+import json
+import os
+from time import sleep
+
+import requests
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+MAX_RETRIES = 10
+TIMEOUT = 90
+
+URL = "https://rag.ki.rtr.at/llm-service/chat"
+HEADERS = {
+    'Authorization': os.getenv("HTTP_AUTHORIZATION"),
+    'Accept': 'text/event-stream',
+    'Content-Type': 'application/json'
+}
+
+results = []
+
+for question in ["Ist der AIA ausserhalb der EU anwendbar?"]:
+    payload = json.dumps({"prompt": question})
+    attempt = 0
+    while attempt < MAX_RETRIES:
+        try:
+            response = requests.request(
+                "POST", URL, headers=HEADERS, data=payload, stream=True, timeout=TIMEOUT
+            )
+            if response.status_code != 200:
+                logging.info("return code " + str(response.status_code))
+                raise requests.exceptions.ConnectionError
+
+            full_response = ""
+            for line in response.iter_lines():
+                if line:
+                    decoded_line = line.decode('utf-8').strip()
+                    if decoded_line.startswith("data:"):
+                        json_data = json.loads(decoded_line[5:].strip())
+                        logging.info("got " + json_data.get("type"))
+                        if json_data.get("type") == "assistant":
+                            exit(0)
+        except (requests.exceptions.ChunkedEncodingError,
+                requests.exceptions.ConnectionError) as e:
+            attempt += 1
+            logging.info("retrying " + str(attempt))
+            sleep(10)
+
+logging.warning("maximum retries reached, exiting")
+exit(1)
